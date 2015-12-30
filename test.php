dc23e2058a324012691cb3c6a802f97614af45b4
william <william@william.com>
2015-12-30 18:53:31 +0800

    fix

--- test.php
+++ test.php
@@ -14,16 +14,16 @@ $sc = $servers[$hash->mHash($key) %2];
 $memcached = new memcached($sc);
 $memcached->set($key,$value);
 //一致性的hash分布
-$serverList = array(
-	$hash->addServer('192.168.1.1'),
-	$hash->addServer('192.168.1.2'),
-	$hash->addServer('192.168.1.3'),
-	$hash->addServer('192.168.1.4'),
-	$hash->addServer('192.168.1.5'),
-	);
+
+
+$hash->addServer('192.168.1.1');
+$hash->addServer('192.168.1.2');
+$hash->addServer('192.168.1.3');
+$hash->addServer('192.168.1.4');
+$hash->addServer('192.168.1.5');
+
 var_dump($hash->lookUp('key1'));
 var_dump($hash->lookUp('key2'));
-$memcached = new memcached();
-$memcached -> connect($serverList[0],$port);
-$memcached->set('key1','value1');
+
+
 ?>
